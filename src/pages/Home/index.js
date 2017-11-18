eee0aad930c4b2ba6d9617c848341b423cd8969a
michaljarnot <michal.jarnot@strv.com>
2017-11-19 00:44:17 +0100

    Update home page
    - list all major dependencies
    - make paragraph text more similar to the one in the github readme

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,17 +13,25 @@ const HomePage = () => (
   <Grid>
     <PageHeader>React Redux Apollo GraphQL Hot Boilerplate 2.0</PageHeader>
     <p>
-      This is yet another react boilerplate. I created first version 9 months ago because it was
-      hard to find up-to-date react, redux and apollo implementation. I made a lot of major updates.
-      I hope this repo will save you some time while learning apollo graphql.
+      Updated version of the old apollo graphql boilerplate. This one is cleaner and functional. I
+      used boilerplate code from my
+      <a href="https://github.com/developer239/workbox-webpack-react"> workbox-webpack-react </a>
+      but I got rid of the PWA aspects
+      because
+      progressive websites are little bit confusing for new developers.
+    </p>
+    <p>
+      You can find the source code
+      <a href="https://github.com/developer239/ReactReduxApolloGraphQLHotBoilerplate"> here</a>.
     </p>
     <h4>Major Updates</h4>
     <h5>Dependencies:</h5>
     <ul>
       <li>functional programming with Recompose 0.26.0</li>
-      <li>Apollo 2.0.2</li>
+      <li>React Apollo 2.0.2</li>
       <li>React 16.1.0</li>
-      <li>Webpack 3.8.1</li>
+      <li>React Router 4.2.2</li>
+      <li>Redux 3.7.2</li>
       <li>Webpack 3.8.1</li>
       <li>Styled Components 2.2.3</li>
     </ul>
