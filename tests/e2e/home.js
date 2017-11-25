fd2499bd66429f0100c8117b0f390e54c66677de
michaljarnot <michal.jarnot@strv.com>
2017-11-25 19:54:42 +0100

    Update first homepage test

--- tests/e2e/home.js
+++ tests/e2e/home.js
@@ -1,4 +1,6 @@
 const clientService = require('../helpers/client')
+const assertHelper = require('../helpers/assert')
+const selectors = require('../selectors')
 
 
 module.exports = {
@@ -6,12 +8,14 @@ module.exports = {
 
   beforeEach: (browser, done) => {
     this.client = clientService(browser)
+    this.assert = assertHelper(browser)
     done()
   },
 
-  'Shows Home Page': () => {
-    this.client
-      .goToPage('/')
-      .end()
+  'Shows Basic Home Page Content': () => {
+    this.client.goToPage('/')
+    this.assert.containsText(selectors.home.header, 'React Redux Apollo GraphQL Hot Boilerplate 2.0')
+    this.client.waitForElementVisible(selectors.home.dummyImg)
+    this.client.end()
   },
 }
