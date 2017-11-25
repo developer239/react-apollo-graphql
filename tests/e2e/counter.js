9f2d4875625e3fc05de131493f503e5d236a5dbf
michaljarnot <michal.jarnot@strv.com>
2017-11-25 20:04:35 +0100

    Write tests for counter page

---
+++ tests/e2e/counter.js
@@ -0,0 +1,41 @@
+const clientService = require('../helpers/client')
+const assertHelper = require('../helpers/assert')
+const selectors = require('../selectors')
+
+
+module.exports = {
+  client: null,
+
+  beforeEach: (browser, done) => {
+    this.client = clientService(browser)
+    this.assert = assertHelper(browser)
+    done()
+  },
+
+  'Required elements exist': () => {
+    this.client.goToPage('/counter')
+    this.assert.containsText(selectors.counter.header, 'Redux Counter')
+    this.client.waitForElementVisible(selectors.counter.value)
+    this.client.waitForElementVisible(selectors.counter.buttonIncrement)
+    this.client.waitForElementVisible(selectors.counter.buttonDecrement)
+  },
+
+  'Can increment value': () => {
+    this.client.goToPage('/counter')
+    this.assert.containsText(selectors.counter.value, '0')
+    this.client.click(selectors.counter.buttonIncrement)
+    this.assert.containsText(selectors.counter.value, '1')
+    this.client.click(selectors.counter.buttonIncrement)
+    this.assert.containsText(selectors.counter.value, '2')
+  },
+
+  'Can decrement value': () => {
+    this.client.goToPage('/counter')
+    this.assert.containsText(selectors.counter.value, '0')
+    this.client.click(selectors.counter.buttonDecrement)
+    this.assert.containsText(selectors.counter.value, '-1')
+    this.client.click(selectors.counter.buttonDecrement)
+    this.assert.containsText(selectors.counter.value, '-2')
+    this.client.end()
+  },
+}
