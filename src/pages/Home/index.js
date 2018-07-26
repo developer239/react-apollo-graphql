9df11741932fb90443aee02106ddf8ea40f2708d
michaljarnot <m.jarnot@yahoo.com>
2018-07-27 00:14:40 +0200

    Update homepage copy

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,20 +4,20 @@ import { H2, P, A } from 'components/Typography'
 
 const HomePage = () => (
   <section>
-    <H2>React Redux Apollo Graphql</H2>
+    <H2>React Apollo GraphQl</H2>
     <P>
-      The third version of this minimal react apollo boilerplate. Originally this application was
-      created a couple of years ago when I was learning GraphQl but couldn’t find many high-quality
-      resources. The situation changed a lot since then but I am trying to keep this repository
-      alive.
+      The third version of minimal react apollo boilerplate. I was learning GraphQl several years
+      ago. However, I couldn't find many react-apollo examples. The situation changed a lot since
+      then, but I am trying to keep this repository alive. There is never enough of simple examples.
+      I hope this one will make learning Apollo and GraphQl easier for you.
     </P>
     <P>
-      If you want to learn graphql I highly recommend this website:
-      <A href="https://www.howtographql.com"> https://www.howtographql.com</A>
+      Backend is provided by
+      <A href="https://www.graph.cool/"> graphq.cool</A>.
     </P>
     <P>
-      You can find the source code of this application
-      <A href="https://github.com/developer239/react-redux-apollo-graphql"> here</A>
+      Link to github repository
+      <A href="https://github.com/developer239/react-redux-apollo-graphql"> is here</A>.
     </P>
   </section>
 )
