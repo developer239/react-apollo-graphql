732f039a61b2468f826bf81e1d44af4f54b62b06
michaljarnot <m.jarnot@yahoo.com>
2018-12-28 22:27:35 +0100

    Remove redundant code

--- __mocks__/AppProvider.js
+++ __mocks__/AppProvider.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { MockedProvider } from 'react-apollo/test-utils'
-import { ThemeProvider } from 'styled-components'
-import { theme } from 'styles'
 import 'jest-styled-components'
 import 'jsdom-global/register'
 
 
 const Provider = ({ mocks, children }) => (
   <MockedProvider mocks={mocks} addTypename={false}>
-    <ThemeProvider theme={theme}>
-      {children}
-    </ThemeProvider>
+    {children}
   </MockedProvider>
 )
 
