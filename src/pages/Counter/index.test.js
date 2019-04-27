2baae44e1e9607aac3dd1b69f0aed84e11612c4f
Michal Jarnot <m.jarnot@yahoo.com>
2019-04-27 16:54:23 +0200

    Write more advanced tests for counter page

--- src/pages/Counter/index.test.js
+++ src/pages/Counter/index.test.js
@@ -1,10 +1,25 @@
 import React from 'react'
-import Page from './index'
+import { waitForElement } from 'react-testing-library'
+import * as Page from './index'
 import { renderApp } from '../../../test/utils/render'
+import { fakeDefaultCounterValue } from '../../../test/utils/gql-counter'
 
-describe('[page] HOME', () => {
+describe('[page] Counter', () => {
   it('should render correctly', () => {
-    const renderer = renderApp(<Page />, '/counter')
+    const renderer = renderApp(<Page.CounterPage />, '/counter')
     expect(renderer.getByTestId('page-counter')).toBeTruthy()
+  })
+
+  it('should show default counter state', async () => {
+    const { getByTestId } = renderApp(<Page.CounterPage />, '/counter', [
+      fakeDefaultCounterValue,
+    ])
+
+    // Wait for Query to be resolved
+    await waitForElement(() => getByTestId('counter-value'))
+
+    expect(getByTestId('counter-value').innerHTML).toEqual(
+      `${fakeDefaultCounterValue.result.data.counterValue}`
+    )
   })
 })
