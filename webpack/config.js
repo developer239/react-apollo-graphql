b08b315096eeafdcbc6c787a7416291034ea1bfb
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-10 05:38:18 +0200

    feat: make config values configurable with env variables

--- webpack/config.js
+++ webpack/config.js
@@ -1,6 +1,6 @@
-const PORT = 3000
-const BUILD_DIR = 'build'
-const BUILD_DIR_PUBLIC = 'public'
+const PORT = process.env.PORT || 3000
+const BUILD_DIR = process.env.BUILD_DIR || 'build'
+const BUILD_DIR_PUBLIC = process.env.BUILD_DIR_PUBLIC || 'public'
 
 module.exports = {
   PORT,
