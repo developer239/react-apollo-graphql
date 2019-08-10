64bf7ae65b9a84c26ce7cefd935d1e4546c9f298
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-10 05:38:17 +0200

    feat: create basic project files

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Hello React,Webpack 4 & Babel 7!</div>;
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
