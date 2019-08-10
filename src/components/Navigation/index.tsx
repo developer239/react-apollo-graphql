f4a22599654f65fa84ee106b47d10cabc108cb3c
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-10 05:38:20 +0200

    feat: create basic nav bar styles

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,14 +1,43 @@
 import React from 'react'
+import { Layout, Menu } from 'antd'
 import { Link } from 'react-router-dom'
-import { AuthButtons } from './AuthButtons'
+import { useQuery } from '@apollo/react-hooks'
+import { Me } from '../../modules/auth/gql/__generated__/Me'
+import { ME_QUERY } from '../../modules/auth/gql'
 
-export const Navigation = () => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-      <AuthButtons />
-    </ul>
-  </div>
-)
+const { Header } = Layout
+
+export const Navigation = () => {
+  const { data } = useQuery<Me>(ME_QUERY)
+
+  const menuItems = [
+    { id: 1, to: '/', label: 'Home' },
+    ...(data && data.me
+      ? [
+          { id: 2, to: '/blog/create', label: 'Create' },
+          { id: 3, to: '/me', label: 'Me' },
+          { id: 4, to: '/logout', label: 'Log Out' },
+        ]
+      : [
+          { id: 5, to: '/register', label: 'Register' },
+          { id: 6, to: '/login', label: 'Login' },
+        ]),
+  ]
+
+  return (
+    <Header>
+      <Menu
+        theme="dark"
+        mode="horizontal"
+        defaultSelectedKeys={['1']}
+        style={{ lineHeight: '65px' }}
+      >
+        {menuItems.map(menuItem => (
+          <Menu.Item key={menuItem.id}>
+            <Link to={menuItem.to}>{menuItem.label}</Link>
+          </Menu.Item>
+        ))}
+      </Menu>
+    </Header>
+  )
+}
