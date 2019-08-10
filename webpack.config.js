bfe9cad1c59aa6d9e23810fc880f9b2f5dfa0403
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-10 05:38:17 +0200

    feat: add html webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 module.exports = {
   module: {
     rules: [
@@ -5,9 +7,23 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
+          loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader'
+          }
+        ]
       }
     ]
-  }
-};
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    })
+  ]
+}
