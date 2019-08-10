6979fb188b648b7b0207e9b519e27c943c4dbedc
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-10 05:38:11 +0200

    Install babel

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+}
