5b690c92a5765977172b81351d84ada5bec80468
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-10 05:38:19 +0200

    feat: make absolute imports work

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,20 @@ module.exports = {
     '@strv/eslint-config-react/optional',
     '@strv/eslint-config-typescript',
     'prettier',
-    'prettier/react'
+    'prettier/react',
   ],
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'paths': [
+          'src',
+        ],
+      },
+    },
+  },
   'plugins': [
     '@typescript-eslint',
-    'react-hooks'
+    'react-hooks',
   ],
   'root': true,
   'env': {
@@ -16,21 +25,21 @@ module.exports = {
     'commonjs': true,
     'es6': true,
     'jest': true,
-    'node': true
+    'node': true,
   },
   'parser': '@typescript-eslint/parser',
   'parserOptions': {
     'ecmaVersion': 2018,
     'sourceType': 'module',
     'ecmaFeatures': {
-      'jsx': true
-    }
+      'jsx': true,
+    },
   },
   'rules': {
     'react/no-did-mount-set-state': 'error',
     'import/no-default-export': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 0,
-    'react/prop-types': 0
-  }
+    'react/prop-types': 0,
+  },
 }
