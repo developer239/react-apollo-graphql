2ba8162a9447cf46c80b41e1553f287f501fbbef
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-10 05:38:09 +0200

    Update dependencies 🌴

--- src/utils/typography.test.js
+++ src/utils/typography.test.js
@@ -1,22 +1,27 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { mount } from 'enzyme'
+import Provider from 'AppProvider'
 import { nl2br } from './typography'
 
 
 describe('Typography', () => {
   describe('nl2br', () => {
     it('renders without any new lines', () => {
-      const wrapper = shallow(
-        <div>{`Some simple text.`}</div>
+      const wrapper = mount(
+        <Provider>
+          <div>{`Some simple text.`}</div>
+        </Provider>
       )
       expect(wrapper).toMatchSnapshot()
     })
 
     it('renders with new lines replaced by br elements', () => {
-      const wrapper = shallow(
-        <div>{nl2br(`Some simple
+      const wrapper = mount(
+        <Provider>
+          <div>{nl2br(`Some simple
 
 text.`)}</div>
+        </Provider>
       )
       expect(wrapper).toMatchSnapshot()
     })
