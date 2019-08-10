596c600f501b536ffa7101aa158c3ec538290a13
Michal Jarnot <m.jarnot@yahoo.com>
2019-08-10 05:38:21 +0200

    feat: make all buttons larger

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,5 +3,7 @@ import { Button as AntButton } from 'antd'
 import { ButtonProps } from 'antd/lib/button'
 
 export const Button: FC<ButtonProps> = ({ children, ...rest }) => (
-  <AntButton {...rest}>{children}</AntButton>
+  <AntButton size="large" {...rest}>
+    {children}
+  </AntButton>
 )
