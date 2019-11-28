9eb19dd5ac34b2a6bfe44a92bb49d157f183bc57
Michal Jarnot <m.jarnot@yahoo.com>
2019-11-28 13:13:38 -0800

    fix: revert intentional errors

--- src/components/Content/index.ts
+++ src/components/Content/index.ts
@@ -2,6 +2,6 @@ import styled from 'styled-components'
 import { Layout } from 'antd'
 
 export const Content = styled(Layout.Content)`
-  min-height: 100%
-  padding: 65px 50px
+  min-height: 100%;
+  padding: 65px 50px;
 `
